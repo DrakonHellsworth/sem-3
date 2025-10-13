6e663f51c780af0f8d83d14beb4f0a520e2ec7c3
Anmol <anmol392005@gmail.com>
2025-10-13 15:09:12 +0530

    Adds MST timing comparison and improves encoding output
    Compares execution times of Prim’s and Kruskal’s algorithms for Minimum Spanning Tree and updates documentation to reflect experiment details and results.
    
    Enhances data encoding output by generating list columns for categorical features, improving readability and downstream analysis.
    
    Facilitates algorithm selection for different graph densities and clarifies experiment outcomes.

--- DAA/exp6.c
+++ DAA/exp6.c
@@ -1,27 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <time.h>
 #define INF 999
 
-// ----------- Prim's Algorithm -----------
-void prim(int n,int G[20][20])
+int par[20];
+
+int find(int i) 
+{
+    while(par[i]!=i)
+    {
+        i=par[i];
+    }
+    return i;
+}
+
+void uni(int i,int j) 
+{
+    int a=find(i);
+    int b=find(j);
+    par[a]=b;
+}
+
+void prim(int n,int G[20][20]) 
 {
     int vis[20]={0};
     vis[0]=1;
     int e=0,tot=0;
     printf("\nEdges in MST (Prim's):\n");
-    while(e<n-1)
+    while(e<n-1) 
     {
         int mn=INF,a=-1,b=-1;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n;i++) 
         {
-            if(vis[i])
+            if(vis[i]) 
             {
-                for(int j=0;j<n;j++)
+                for(int j=0;j<n;j++) 
                 {
-                    if(!vis[j] && G[i][j])
+                    if(!vis[j] && G[i][j]) 
                     {
-                        if(G[i][j]<mn)
+                        if(G[i][j]<mn) 
                         {
                             mn=G[i][j];
                             a=i;
@@ -36,44 +52,25 @@ void prim(int n,int G[20][20])
         vis[b]=1;
         e++;
     }
-    printf("Total Weight = %d\n",tot);
+    printf("Total Weight=%d\n",tot);
 }
 
-// ----------- Kruskal's Algorithm -----------
-int par[20];
-
-int find(int i)
+void kruskal(int n,int G[20][20]) 
 {
-    while(par[i]!=i)
-    {
-        i=par[i];
-    }
-    return i;
-}
-
-void uni(int i,int j)
-{
-    int a=find(i);
-    int b=find(j);
-    par[a]=b;
-}
-
-void kruskal(int n,int G[20][20])
-{
-    for(int i=0;i<n;i++)
+    for(int i=0;i<n;i++) 
     {
         par[i]=i;
     }
     int e=0,tot=0;
     printf("\nEdges in MST (Kruskal's):\n");
-    while(e<n-1)
+    while(e<n-1) 
     {
         int mn=INF,a=-1,b=-1;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n;i++) 
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<n;j++) 
             {
-                if(find(i)!=find(j) && G[i][j]<mn)
+                if(find(i)!=find(j) && G[i][j]<mn) 
                 {
                     mn=G[i][j];
                     a=i;
@@ -82,33 +79,47 @@ void kruskal(int n,int G[20][20])
             }
         }
         uni(a,b);
-        printf("%d - %d : %d\n",a,b,mn);
+        printf("%d-%d:%d\n",a,b,mn);
         tot+=mn;
         e++;
     }
-    printf("Total Weight = %d\n",tot);
+    printf("Total Weight=%d\n",tot);
 }
 
-// ----------- Main Function -----------
-int main()
+int main() 
 {
     int n;
     printf("Enter number of vertices:");
     scanf("%d",&n);
     int G[20][20];
     printf("Enter adjacency matrix:\n");
-    for(int i=0;i<n;i++)
+    for(int i=0;i<n;i++) 
     {
-        for(int j=0;j<n;j++)
+        for(int j=0;j<n;j++) 
         {
             scanf("%d",&G[i][j]);
-            if(G[i][j]==0)
+            if(G[i][j]==0) 
             {
-                G[i][j]=INF; // treat 0 as no edge
+                G[i][j]=INF;
             }
         }
     }
+
+    clock_t s,e;
+    double t1,t2;
+
+    s=clock();
     prim(n,G);
+    e=clock();
+    t1=(double)(e-s)/CLOCKS_PER_SEC;
+
+    s=clock();
     kruskal(n,G);
+    e=clock();
+    t2=(double)(e-s)/CLOCKS_PER_SEC;
+
+    printf("\nTime taken for Prim's algorithm: %lf sec\n",t1);
+    printf("Time taken for Kruskal's algorithm: %lf sec\n",t2);
+
     return 0;
 }
